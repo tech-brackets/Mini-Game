c9343c189725eb6b7ba9d43ae234f14ed097255f
RezhaIkhwan.H <rezhaihwanhidayat97@gmail.com>
2022-10-09 14:08:10 +0700

    add fix bug flpy

--- game flypy js/script.js
+++ game flypy js/script.js
@@ -4,13 +4,12 @@ const hole = document.querySelector('#hole');
 let jumping = 0;
 let counter = 0;
 var loop = [];
-Window.score = 0;
 
 hole.addEventListener("animationiteration", function () {
     let random = -(Math.random() * 300 + 150);
+    console.log(random);
     hole.style.top = random + "px";
     counter++
-
 })
 
 setInterval(() => {
@@ -20,18 +19,16 @@ setInterval(() => {
     const holebotom = holetop + (150 - parseInt(window.getComputedStyle(carakter).getPropertyValue("height")))
     const score = document.getElementsByClassName("score")[0];
     if (jumping == 0) {
-        carakter.style.top = caraktertop + 3 + 'px'
+        carakter.style.top = caraktertop + 4 + 'px'
     }
 
-
     if ((caraktertop > 480) || ((blockleft < 20) && (blockleft > -50) && ((ctop < holetop) || (ctop > holebotom)))) {
-        alert(`YOU DEAT!! \n youre score: ${+Window.score}`)
+        alert(`YOU DEAT!! \n youre score: ${counter}`)
         window.location.reload();
         carakter.style.top = 100 + 'px'
         counter = 0
     }
-    score.textContent = Window.score;
-    Window.score++;
+    score.innerHTML = counter;
 
 }, 10);
 
@@ -40,30 +37,37 @@ function jum() {
     let jumcount = 0
     let juminterval = setInterval(() => {
         let caraktertop = parseInt(window.getComputedStyle(carakter).getPropertyValue("top"));
-        if ((caraktertop > 6) && (jumcount < 10)) {
-            carakter.style.top = caraktertop - 0.01 + 'px'
-        }
+        if ((caraktertop > 6) && (jumcount < 10)) carakter.style.top = caraktertop - 0.01 + 'px'
 
         if (jumcount >= 10) {
             clearInterval(juminterval)
             jumping = 0;
             jumcount = 0;
         }
+
         jumcount++
+
     }, 10);
+
 }
 
+this.clearInterval
 window.addEventListener('keydown', function (e) {
+
     if (e.code == 'Space') {
         jum()
         loop.push(setInterval(() => {
             jum()
         }, 10))
     }
+
+
 })
 
 window.addEventListener('keyup', function (e) {
     if (e.code == 'Space') {
         loop.map(e => this.clearInterval(e))
     }
+
+
 })
